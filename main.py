1af8d6344bcafcf9c972aa807db509697a669616
Tomsh666 <tomshinskiy02@gmail.com>
2024-05-20 15:32:47 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+def main():
+    print("1.The case of short messages")
+    print("2.RSA-OAEP")
+
+    choice = input("Select an option:")
+    if choice == "1":
+
+
+
+
+
+
+
+        print("Done")
+    elif choice == "2":
+        print("Done")
+    else:
+        print("Wrong option")
+
+
+if __name__ == '__main__':
+    main()
+
